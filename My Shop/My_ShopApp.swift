de9f422d226756ed4a00833931f5e32ede3bc164
amanullah <amanullah.sarker@amiprobashi.com>
2024-06-10 13:44:19 +0600

    Initial Commit

---
+++ My Shop/My_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_ShopApp.swift
+//  My Shop
+//
+//  Created by Amanullah Sarker on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct My_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ My ShopUITests/My_ShopUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  My_ShopUITestsLaunchTests.swift
+//  My ShopUITests
+//
+//  Created by Amanullah Sarker on 10/6/24.
+//
+
+import XCTest
+
+final class My_ShopUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
